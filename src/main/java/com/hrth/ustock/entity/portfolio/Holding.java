1cdef1b6d283761093b53b2b6590114a40a49d84
Lcrescent3es <crescent3es@gmail.com>
2024-08-21 00:55:42 +0900

    Fix: Holding entity에 User 추가

--- src/main/java/com/hrth/ustock/entity/portfolio/Holding.java
+++ src/main/java/com/hrth/ustock/entity/portfolio/Holding.java
@@ -1,5 +1,6 @@
 package com.hrth.ustock.entity.portfolio;
 
+import com.hrth.ustock.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,22 +23,26 @@ public class Holding {
     @JoinColumn(name = "pf_id")
     private Portfolio portfolio;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "stock_code")
     private Stock stock;
 
-    private int quantity;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    private int average;
+    private long quantity;
 
-    public void additionalBuyHolding(int quantity, int price) {
-        long total = (long) this.quantity * this.average;
-        total += (long) quantity * price;
+    private long average;
+
+    public void additionalBuyHolding(long quantity, long price) {
+        long total = this.quantity * this.average;
+        total += quantity * price;
         this.quantity += quantity;
-        this.average = (int) (total / this.quantity);
+        this.average = total / this.quantity;
     }
 
-    public void updateHolding(int quantity, int price) {
+    public void updateHolding(long quantity, long price) {
         this.quantity = quantity;
         this.average = price;
     }
