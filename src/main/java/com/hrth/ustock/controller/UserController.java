eeb42c65eb3b270f4b68981b45f18fcabbf133d3
SeungBeom Yu <74458310+sngbmYu@users.noreply.github.com>
2024-08-21 19:32:32 +0900

    Fix: URL 리소스명 수정

--- src/main/java/com/hrth/ustock/controller/UserController.java
+++ src/main/java/com/hrth/ustock/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/v1/userInfo")
+@RequestMapping("/v1/user")
 public class UserController {
     @GetMapping
     public ResponseEntity<?> getUserInfo(Authentication authentication) {
