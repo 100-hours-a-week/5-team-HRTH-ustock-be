3b70a7a49d934f00b5c8464ae4be53e44c0644a6
yu seungbeom <dldkdlelqnzo@gmail.com>
2024-08-22 15:50:07 +0900

    Fix: 종목 검색시 정렬 기준이 잘못 되었던 문제 수정

--- src/main/java/com/hrth/ustock/service/StockService.java
+++ src/main/java/com/hrth/ustock/service/StockService.java
@@ -61,12 +61,13 @@ public class StockService {
 
     // 6. 종목 검색
     @Transactional
-    public List<StockDto> findByStockName(String str) {
+    public List<StockDto> findByStockName(String name) {
 
-        List<Stock> list = stockRepository.findAllByNameContaining(str);
+        List<Stock> list = stockRepository.findByNameStartingWith(name);
+        list.addAll(stockRepository.findByNameContainingButNotStartingWith(name));
 
         // TODO: 현재가, 등락율 추가
-        if (list == null || list.isEmpty()) {
+        if (list.isEmpty()) {
             throw new StockNotFoundException();
         }
 
