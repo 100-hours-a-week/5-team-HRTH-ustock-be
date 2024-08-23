db074ebf22c974d1d0f551550ec43e172f458009
Lcrescent3es <crescent3es@gmail.com>
2024-08-23 18:49:36 +0900

    Feat: 크론잡 3가지 구현(상수 추가)

--- src/main/java/com/hrth/ustock/service/StockServiceConst.java
+++ src/main/java/com/hrth/ustock/service/StockServiceConst.java
@@ -12,7 +12,24 @@ public interface StockServiceConst {
     String STOCK_NAME = "hts_kor_isnm";
     String STOCK_CODE = "mksc_shrn_iscd";
     String CHANGE_STOCK_CODE = "stck_shrn_iscd";
+
+    // 종목 차트, 현재가, 전일 대비, 전일 대비율
+    String STOCK_HIGH = "stck_hgpr";
+    String STOCK_LOW = "stck_lwpr";
+    String STOCK_OPEN = "stck_oprc";
+    String STOCK_CLOSE = "stck_clpr";
     String STOCK_CURRENT_PRICE = "stck_prpr";
     String CHANGE_FROM_PREVIOUS_STOCK = "prdy_vrss";
     String CHANGE_RATE_FROM_PREVIOUS_STOCK = "prdy_ctrt";
+
+    // redis 저장 키
+    String REDIS_CURRENT_KEY = "current";
+    String REDIS_CHANGE_KEY = "change";
+    String REDIS_CHANGE_RATE_KEY = "changeRate";
+    String REDIS_CHART_KEY = "chart";
+    String REDIS_CHART_OPEN_KEY = "open";
+    String REDIS_CHART_CLOSE_KEY = "close";
+    String REDIS_CHART_HIGH_KEY = "high";
+    String REDIS_CHART_LOW_KEY = "low";
+    String REDIS_CHART_DATE_KEY = "date";
 }
