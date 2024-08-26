bdc79fb945f3c36079f64daf6b3a8c0819ccb79b
Lcrescent3es <crescent3es@gmail.com>
2024-08-26 10:53:50 +0900

    Chore: cors 설정 되돌림

---
+++ src/main/java/com/hrth/ustock/config/CorsMvcConfig.java
@@ -0,0 +1,25 @@
+package com.hrth.ustock.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+    @Value("${spring.config.url}")
+    private String url;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .exposedHeaders("Set-Cookie")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins(url)
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600L);
+    }
+}
