b5bfffbeb85e022f823a5918d0626a9b93de0c8f
yu seungbeom <dldkdlelqnzo@gmail.com>
2024-09-02 21:48:35 +0900

    Fix: Controller Swagger 설정 변경

--- src/main/java/com/hrth/ustock/controller/main/PortfolioController.java
+++ src/main/java/com/hrth/ustock/controller/main/PortfolioController.java
@@ -7,9 +7,6 @@ import com.hrth.ustock.dto.main.portfolio.PortfolioResponseDto;
 import com.hrth.ustock.service.auth.CustomUserService;
 import com.hrth.ustock.service.main.PortfolioService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -26,13 +23,7 @@ public class PortfolioController {
 
     @GetMapping
     @Operation(summary = "보유 포트폴리오 리스트 조회", description = "사용자가 보유중인 포트폴리오 리스트 반환")
-    @ApiResponse(responseCode = "200",
-            content = @Content(
-                    mediaType = "application/json",
-                    schema = @Schema(implementation = PortfolioListDto.class)
-            )
-    )
-    public ResponseEntity<?> showPortfolioList() {
+    public ResponseEntity<PortfolioListDto> showPortfolioList() {
 
         PortfolioListDto list = portfolioService.getPortfolioList(customUserService.getCurrentUserDetails().getUserId());
 
@@ -41,7 +32,7 @@ public class PortfolioController {
 
     @PostMapping
     @Operation(summary = "포트폴리오 생성", description = "사용자의 새 포트폴리오 생성")
-    public ResponseEntity<?> createPortfolio(@RequestBody PortfolioRequestDto portfolioRequestDto) {
+    public ResponseEntity<Void> createPortfolio(@RequestBody PortfolioRequestDto portfolioRequestDto) {
 
         portfolioService.addPortfolio(portfolioRequestDto, customUserService.getCurrentUserDetails().getUserId());
 
@@ -50,7 +41,7 @@ public class PortfolioController {
 
     @GetMapping("/check")
     @Operation(summary = "포트폴리오 보유 여부 확인", description = "사용자의 포트폴리오 보유 여부 반환")
-    public ResponseEntity<?> checkUserHasPortfolio() {
+    public ResponseEntity<Void> checkUserHasPortfolio() {
 
         portfolioService.getPortfolioList(customUserService.getCurrentUserDetails().getUserId());
 
@@ -59,13 +50,7 @@ public class PortfolioController {
 
     @GetMapping("/{pfId}")
     @Operation(summary = "포트폴리오 조회", description = "사용자의 특정 포트폴리오 조회")
-    @ApiResponse(responseCode = "200",
-            content = @Content(
-                    mediaType = "application/json",
-                    schema = @Schema(implementation = PortfolioResponseDto.class)
-            )
-    )
-    public ResponseEntity<?> showPortfolioById(@PathVariable("pfId") Long pfId) {
+    public ResponseEntity<PortfolioResponseDto> showPortfolioById(@PathVariable("pfId") Long pfId) {
         PortfolioResponseDto portfolio = portfolioService.getPortfolio(pfId);
 
         return ResponseEntity.ok().body(portfolio);
@@ -73,7 +58,7 @@ public class PortfolioController {
 
     @PatchMapping("/{pfId}/holding/{code}")
     @Operation(summary = "종목 추가 매수", description = "특정 포트폴리오 보유종목 추가매수")
-    public ResponseEntity<?> buyAdditionalPortfolioStock(
+    public ResponseEntity<Void> buyAdditionalPortfolioStock(
             @PathVariable("pfId") Long pfId, @PathVariable("code") String code, @RequestBody HoldingRequestDto holdingRequestDto) {
 
         portfolioService.additionalBuyStock(pfId, code, holdingRequestDto);
@@ -83,7 +68,7 @@ public class PortfolioController {
 
     @PutMapping("/{pfId}/holding/{code}")
     @Operation(summary = "종목 수정", description = "특정 포트폴리오 보유종목 수정")
-    public ResponseEntity<?> editPortfolioStock(
+    public ResponseEntity<Void> editPortfolioStock(
             @PathVariable("pfId") Long pfId, @PathVariable("code") String code, @RequestBody HoldingRequestDto holdingRequestDto) {
 
         portfolioService.editHolding(pfId, code, holdingRequestDto);
@@ -93,7 +78,7 @@ public class PortfolioController {
 
     @DeleteMapping("/{pfId}/holding/{code}")
     @Operation(summary = "보유 종목 삭제", description = "특정 포트폴리오 보유종목 삭제")
-    public ResponseEntity<?> deletePortfolioStock(@PathVariable("pfId") Long pfId, @PathVariable("code") String code) {
+    public ResponseEntity<Void> deletePortfolioStock(@PathVariable("pfId") Long pfId, @PathVariable("code") String code) {
 
         portfolioService.deleteHolding(pfId, code);
 
@@ -102,7 +87,7 @@ public class PortfolioController {
 
     @DeleteMapping("/{pfId}")
     @Operation(summary = "포트폴리오 삭제", description = "특정 포트폴리오 삭제")
-    public ResponseEntity<?> deletePortfolio(@PathVariable("pfId") Long pfId) {
+    public ResponseEntity<Void> deletePortfolio(@PathVariable("pfId") Long pfId) {
 
         portfolioService.deletePortfolio(pfId);
 
@@ -111,7 +96,7 @@ public class PortfolioController {
 
     @PostMapping("/{pfId}/holding/{code}")
     @Operation(summary = "종목 매수", description = "특정 포트폴리오 새 종목 매수")
-    public ResponseEntity<?> buyPortfolioStock(
+    public ResponseEntity<Void> buyPortfolioStock(
             @PathVariable("pfId") Long pfId, @PathVariable("code") String code, @RequestBody HoldingRequestDto holdingRequestDto) {
 
         portfolioService.buyStock(pfId, code, holdingRequestDto);
