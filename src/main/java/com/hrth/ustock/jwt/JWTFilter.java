77fd59224e5286c7fa6f650ddc2d17b2497e2085
Lcrescent3es <crescent3es@gmail.com>
2024-09-03 14:02:57 +0900

    Fix: 필터 level 전역 예외처리 오류로 인해 JWTFilter revert #127

--- src/main/java/com/hrth/ustock/jwt/JWTFilter.java
+++ src/main/java/com/hrth/ustock/jwt/JWTFilter.java
@@ -2,7 +2,6 @@ package com.hrth.ustock.jwt;
 
 import com.hrth.ustock.dto.oauth2.CustomOAuth2User;
 import com.hrth.ustock.dto.oauth2.UserOauthDto;
-import com.hrth.ustock.exception.domain.user.UserException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
@@ -18,16 +17,15 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.concurrent.TimeUnit;
-
-import static com.hrth.ustock.exception.domain.user.UserExceptionType.REFRESH_NOT_VALID;
 
 @Slf4j
 @RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
     public static final long ACCESS_EXPIRE = 600000L;
-    public static final long REFRESH_EXPIRE = 2592000000L;
-    public static final int COOKIE_EXPIRE = 2592000;
+    public static final long REFRESH_EXPIRE = 86400000L;
+    public static final int COOKIE_EXPIRE = 360000;
 
     private final String domain;
     private final JWTUtil jwtUtil;
@@ -45,47 +43,70 @@ public class JWTFilter extends OncePerRequestFilter {
 
         String access = null;
         String refresh = null;
-        Cookie refreshCookie = null;
         for (Cookie cookie : cookies) {
             if (cookie.getName().equals("access")) {
                 access = cookie.getValue();
             }
             if (cookie.getName().equals("refresh")) {
-                refreshCookie = cookie;
                 refresh = cookie.getValue();
             }
         }
 
-        if ((access == null || access.isEmpty() || jwtUtil.isExpired(access))
-                && (refresh == null || refresh.isEmpty() || jwtUtil.isExpired(refresh))) {
+        if ((access == null || access.isEmpty()) && (refresh == null || refresh.isEmpty())) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        if (!jwtUtil.getCategory(refresh).equals("refresh")) {
-            throw new UserException(REFRESH_NOT_VALID);
+        // access token 소멸시간 검증
+        if (!(access == null || access.isEmpty()) && !jwtUtil.isExpired(access)) {
+            String category = jwtUtil.getCategory(access);
+
+            if (!category.equals("access")) {
+                log.info("access category not match, url: {}", request.getRequestURL());
+                PrintWriter writer = response.getWriter();
+                writer.print("access category not match");
+
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        } else {
+            // Access Token이 만료된 경우 Refresh Token으로 재발급
+            if (refresh == null || jwtUtil.isExpired(refresh) || !jwtUtil.getCategory(refresh).equals("refresh")) {
+                log.info("refresh not valid, url: {}", request.getRequestURL());
+                PrintWriter writer = response.getWriter();
+                writer.print("refresh not valid");
+
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+
+            Long userId = jwtUtil.getUserId(refresh);
+            String provider = jwtUtil.getProvider(refresh);
+            String providerId = jwtUtil.getProviderId(refresh);
+            String providerName = jwtUtil.getProviderName(refresh);
+            String providerImage = jwtUtil.getProviderImage(refresh);
+            String role = jwtUtil.getRole(refresh);
+
+            // Redis에서 해당 Refresh Token이 유효한지 확인
+            String isLogout = (String) redisTemplate.opsForValue().get("RT:" + userId);
+            if (ObjectUtils.isEmpty(isLogout)) {
+                log.info("redis token not match, url: {}", request.getRequestURL());
+                PrintWriter writer = response.getWriter();
+                writer.print("redis token not match");
+
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+
+            String newAccessToken = jwtUtil.createJwt("access", userId, provider, providerId, providerName, providerImage, role, ACCESS_EXPIRE);
+            String newRefreshToken = jwtUtil.createJwt("refresh", userId, provider, providerId, providerName, providerImage, role, REFRESH_EXPIRE);
+
+            redisTemplate.opsForValue().set("RT:" + userId, newRefreshToken, REFRESH_EXPIRE, TimeUnit.MILLISECONDS);
+
+            response.addCookie(createCookie("access", newAccessToken));
+            response.addCookie(createCookie("refresh", newRefreshToken));
+            access = newAccessToken;
         }
-
-        Long userId = jwtUtil.getUserId(refresh);
-        String redisResult = (String) redisTemplate.opsForValue().get("RT:" + userId);
-        if (ObjectUtils.isEmpty(redisResult) || !redisResult.equals(refresh)) {
-            throw new ServletException("JWT Refresh 토큰 갱신 필요", new UserException(REFRESH_NOT_VALID));
-        }
-
-        String provider = jwtUtil.getProvider(refresh);
-        String providerId = jwtUtil.getProviderId(refresh);
-        String providerName = jwtUtil.getProviderName(refresh);
-        String providerImage = jwtUtil.getProviderImage(refresh);
-        String role = jwtUtil.getRole(refresh);
-
-        String newAccessToken = jwtUtil.createJwt("access", userId, provider, providerId, providerName, providerImage, role, ACCESS_EXPIRE);
-        String newRefreshToken = jwtUtil.createJwt("refresh", userId, provider, providerId, providerName, providerImage, role, REFRESH_EXPIRE);
-
-        redisTemplate.opsForValue().set("RT:" + userId, newRefreshToken, REFRESH_EXPIRE, TimeUnit.MILLISECONDS);
-
-        response.addCookie(createCookie("access", newAccessToken));
-        response.addCookie(createCookie("refresh", newRefreshToken));
-        access = newAccessToken;
 
         UserOauthDto userOauthDTO = UserOauthDto.builder()
                 .userId(jwtUtil.getUserId(access))
