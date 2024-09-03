1d283477b54f95cdb9be5454adbb76b7a7d5bcba
Lcrescent3es <crescent3es@gmail.com>
2024-09-03 17:10:38 +0900

    Fix: 크론 컨트롤러 수정

--- src/main/java/com/hrth/ustock/controller/common/CronController.java
+++ src/main/java/com/hrth/ustock/controller/common/CronController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@Profile("prod")
+@Profile("prod")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/scheduler")
@@ -18,27 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class CronController {
     private final StockCronService stockCronService;
 
-//    // 주중 오전 9시에 시작해서 30분마다 실행하고 오후 16시 15분에 끝남
-//    // 현재가, 등락, 등락율, 날짜를 redis에 저장
-//    @Scheduled(cron = "0 */15 8-16 * * MON-SAT")
-//    public void setChartData() {
-//        stockCronService.saveStockData();
-//    }
-//
-//    // 5분마다 코스피, 코스닥 데이터 받아옴
-//    @Scheduled(cron = "0 */5 * * * *")
-//    public void setMarketData() {
-//        stockCronService.saveMarketData();
-//    }
-//
-//    // 화-토 오전 9시 10분에 전일(수정주가) 차트 데이터 mysql에 저장
-//    @Scheduled(cron = "0 10 09 ? * TUE-SAT")
-//    public void setEditedChartData() {
-//        stockCronService.saveEditedChartData();
-//    }
+    // 주중 오전 9시에 시작해서 30분마다 실행하고 오후 16시 15분에 끝남
+    // 현재가, 등락, 등락율, 날짜를 redis에 저장
+    @Scheduled(cron = "0 */15 8-16 * * MON-SAT")
+    public void setChartData() {
+        stockCronService.saveStockData();
+    }
 
-    @GetMapping("/test/3")
-    public void setEditedChartDataTest() {
+    // 5분마다 코스피, 코스닥 데이터 받아옴
+    @Scheduled(cron = "0 */5 * * * *")
+    public void setMarketData() {
+        stockCronService.saveMarketData();
+    }
+
+    // 화-토 오전 9시 10분에 전일(수정주가) 차트 데이터 mysql에 저장
+    @Scheduled(cron = "0 10 09 ? * TUE-SAT")
+    public void setEditedChartData() {
         stockCronService.saveEditedChartData();
     }
 }
