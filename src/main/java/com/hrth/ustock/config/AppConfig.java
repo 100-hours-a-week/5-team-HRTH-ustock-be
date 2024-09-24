fd8715caf857c559bf288a9a4f92af1911d2dd9e
Lcrescent3es <crescent3es@gmail.com>
2024-09-24 14:26:14 +0900

    Feat: Kis API 요청횟수 초당 19회로 제한

--- src/main/java/com/hrth/ustock/config/AppConfig.java
+++ src/main/java/com/hrth/ustock/config/AppConfig.java
@@ -1,12 +1,13 @@
 package com.hrth.ustock.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hrth.ustock.util.DateConverter;
-import com.hrth.ustock.util.RedisJsonManager;
-import com.hrth.ustock.util.RedisTTLCalculator;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import java.time.Duration;
+
 
 @Configuration
 public class AppConfig {
@@ -19,17 +20,13 @@ public class AppConfig {
     }
 
     @Bean
-    public DateConverter dateConverter() {
-        return new DateConverter();
-    }
-
-    @Bean
-    public RedisTTLCalculator redisTTLCalculator() {
-        return new RedisTTLCalculator();
-    }
-
-    @Bean
-    public RedisJsonManager redisJsonManager() {
-        return new RedisJsonManager(new ObjectMapper());
+    public RateLimiter kisApiRateLimiter() {
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .limitRefreshPeriod(Duration.ofSeconds(1))
+                .limitForPeriod(19)
+                .timeoutDuration(Duration.ofSeconds(1))
+                .build();
+        RateLimiterRegistry rateLimiterRegistry = RateLimiterRegistry.of(config);
+        return rateLimiterRegistry.rateLimiter("kisApiRateLimiter", config);
     }
 }
